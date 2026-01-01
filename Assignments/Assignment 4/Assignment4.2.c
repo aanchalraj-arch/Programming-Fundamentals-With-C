075c64b73aef0829bebe68a0ee390742a10f3914
Aanchal Raj <aanchalrajindia@gmail.com>
2026-01-01 22:19:40 +0530

    Add files via upload

---
+++ Assignments/Assignment 4/Assignment4.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int findMax(int *a, int *b)
+{
+    if (*a > *b)
+        return *a;
+    else
+        return *b;
+}
+
+int main()
+{
+    int x, y, max;
+
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &x, &y);
+
+    max = findMax(&x, &y);
+
+    printf("Maximum of %d and %d is: %d\n", x, y, max);
+
+    return 0;
+}
